d1e9a11da6f098f2e1749da64ea7f7dc4f946038
VeranikaBabik <veranikababik@gmail.com>
2023-04-17 00:24:07 +0200

    exercise 2

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,15 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+ingredients.forEach((ingredient) => {
+  const item = document.createElement("li");
+  item.textContent = ingredient;
+  const ul = document.querySelector("#ingredients");
+  item.classList.add("item");
+  ul.append(item);
+});
