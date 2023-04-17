4335597c81c9e97806a32469e75c977722f82876
VeranikaBabik <veranikababik@gmail.com>
2023-04-18 01:47:21 +0200

    exercise 8

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,13 @@
-
+const form = document.querySelector(".login-form");
+form.addEventListener("submit", formSubmit);
+function formSubmit(event) {
+  event.preventDefault();
+  const {
+    elements: { email, password },
+  } = event.currentTarget;
+  if (email.value === "" || password.value === "") {
+    return alert("Please, fill in all the fields");
+  }
+  console.log(`Email: ${email.value}, password: ${password.value}`);
+  event.currentTarget.reset();
+}
