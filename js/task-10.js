2602c8b7108d72511f24a1095baa4ef8524e5896
VeranikaBabik <veranikababik@gmail.com>
2023-04-18 03:12:18 +0200

    exercise 10

--- js/task-10.js
+++ js/task-10.js
@@ -3,3 +3,22 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+const number = document.querySelector("input");
+const boxes = document.querySelector("#boxes");
+const btnCreate = document.querySelector("button[data-create]");
+const btnDestroy = document.querySelector("button[data-destroy]");
+function createBoxes(amount) {
+  amount = number.value;
+  for (let i = 0; i < amount; i++) {
+    const size = 30 + i * 10;
+    const box = document.createElement("div");
+    box.style.cssText = `width: ${size}px; height: ${size}px`;
+    box.style.backgroundColor = getRandomHexColor();
+    boxes.prepend(box);
+  }
+}
+function destroyBoxes() {
+  boxes.innerHTML = "";
+}
+btnCreate.addEventListener("click", createBoxes);
+btnDestroy.addEventListener("click", destroyBoxes);
